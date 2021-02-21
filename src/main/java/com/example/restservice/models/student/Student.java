86d5ff43b8688937e671fd1fccf2f42653e41341
Pronob <pronobdas2k16@gmail.com>
2021-02-21 17:28:33 +0600

    Student model created

--- src/main/java/com/example/restservice/models/student/Student.java
+++ src/main/java/com/example/restservice/models/student/Student.java
@@ -5,7 +5,10 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 // Create a model by using the collection name as your Intern ID
 // as example if your Intern ID is int212b003 then the collection name as follow
-@Document(collection = "int212b003")
+@Document(collection = "int212b066")
 public class Student {
-
+    private String name;
+    private String email;
+    private int contact_no;
+    private String DoB;
 }
